1cf1857f6e2ac92a086d6cfa2c289541be8ca9a0
md678685 <mathew678@hotmail.co.uk>
2018-08-03 21:00:55 +0000

    Allow "surround" material names in compound aliases
    Allows stripped wood/logs to be supported.

--- src/items/aliases/compound.js
+++ src/items/aliases/compound.js
@@ -1,23 +1,56 @@
 const _ = require("lodash");
 
 function test({ name }) {
-    return Object.keys(this.prefixes)
-        .reduce((acc, prefix) => acc || name.includes(prefix), false)
-    && Object.keys(this.suffixes)
-        .reduce((acc, suffix) => acc || name.includes(suffix), false);
+    return (includesKeys(name, this.prefixes) && includesKeys(name, this.suffixes))
+        || matchesSurround(name, this);
 }
 
 function get({ name }) {
     const prefix = Object.keys(this.prefixes)
-        .filter(color => name.includes(color))[0]; // always first match, eg. prefers LIGHT_BLUE over BLUE
+        .filter(p => name.includes(p))[0]; // always first match, eg. prefers LIGHT_BLUE over BLUE
     const suffix = Object.keys(this.suffixes)
-        .filter(type => name.includes(type))[0];
+        .filter(s => name.includes(s))[0];
 
     return _.flatten(
         this.prefixes[prefix].map(prefix =>
             this.suffixes[suffix].map(suffix =>
                 joinAlias(prefix, suffix))))
         .filter(alias => !this.protected.includes(alias));
+}
+
+function includesKeys(string, obj) {
+    return Object.keys(obj)
+        .reduce((acc, key) => acc || string.includes(key), false);
+}
+
+function matchesSurround(string, data) {
+    if (!data.combined) {
+        combineSurround(data);
+    }
+    
+    return Object.keys(data.combined).includes(string);
+}
+
+function combineSurround(data) {
+    data.combined = {};
+    const { prefixes, suffixes } = data;
+    Object.keys(suffixes).forEach(suffixKey => {
+        if (suffixKey.includes("$")) {
+            Object.keys(prefixes).forEach(prefixKey => {
+                let key = suffixKey.replace("$", prefixKey);
+                let value = _.flatten(
+                    suffixes[suffixKey].map(suffix => 
+                        prefixes[prefixKey].map(prefix =>
+                            joinAlias(prefix, suffix)
+                        )
+                    )
+                )
+                data.combined[key] = value;
+                console.log(`${key}: ${value}`);
+            });
+        }
+    });
+    console.log(data);
 }
 
 function joinAlias(prefix, suffix) {
