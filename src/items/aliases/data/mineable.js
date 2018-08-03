c56eb944ca6d7ea9ebed2c9982f05f194e23deaa
md678685 <mathew678@hotmail.co.uk>
2018-08-03 21:56:13 +0000

    Add tools/armor to mineable aliases

--- src/items/aliases/data/mineable.js
+++ src/items/aliases/data/mineable.js
@@ -7,6 +7,11 @@ const prefixes = {
     DIAMOND: ["crystal", "diamond", "d"],
     EMERALD: ["emerald", "e"],
     REDSTONE: ["redstone", "reds", "red", "rstone", "rs", "r"],
+    // While not ores, these have armor and tools
+    LEATHER: ["leather", "l"],
+    CHAINMAIL: ["chainmail", "chainm", "cmail", "chain", "cm"],
+    WOODEN: ["wooden", "wood", "w"],
+    STONE: ["cobblestone", "cstone", "cs", "stone", "s"],
 };
 
 // Types of material
@@ -14,6 +19,17 @@ const suffixes = {
     ORE: ["ore", "o", "!ore", "!o"],
     BLOCK: ["block", "!block"],
     INGOT: ["ingot", "bar", "i", "!ingot", "!bar", "!i"],
+    // Tools
+    SWORD: ["sword"],
+    SHOVEL: ["shovel", "spade"],
+    PICKAXE: ["pickaxe", "pick"],
+    AXE: ["axe"],
+    HOE: ["hoe"],
+    // Armor
+    HELMET: ["helmet", "helm", "hat", "coif"],
+    CHESTPLATE: ["chestplate", "platebody", "plate", "shirt", "tunic"],
+    LEGGINGS: ["leggings", "legs", "pants"],
+    BOOTS: ["boots", "shoes"],
 }
 
 // Ambiguous aliases that shouldn't get outputted here
