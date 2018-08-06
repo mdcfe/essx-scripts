90378a364212b8d9b1df16d455fcbad3e260c16c
md678685 <mathew678@hotmail.co.uk>
2018-08-06 10:39:12 +0100

    Flatten potions before returning

--- src/items/materials/potions.js
+++ src/items/materials/potions.js
@@ -1,36 +1,40 @@
+const _ = require("lodash");
+
 const potionList = require("./data/potions.json");
 
 const retrieve = () => {
-    return potionList.map(potion => [
-        {
-            name: "POTION",
-            skipSimple: true,
-            meta: {
-                potion
-            }
-        },
-        {
-            name: "SPLASH_POTION",
-            skipSimple: true,
-            meta: {
-                potion
-            }
-        },
-        {
-            name: "LINGERING_POTION",
-            skipSimple: true,
-            meta: {
-                potion
-            }
-        },
-        {
-            name: "TIPPED_ARROW",
-            skipSimple: true,
-            meta: {
-                potion
-            }
-        },
-    ]);
+    return _.flatten(
+        potionList.map(potion => [
+            {
+                name: "POTION",
+                skipSimple: true,
+                meta: {
+                    potion
+                }
+            },
+            {
+                name: "SPLASH_POTION",
+                skipSimple: true,
+                meta: {
+                    potion
+                }
+            },
+            {
+                name: "LINGERING_POTION",
+                skipSimple: true,
+                meta: {
+                    potion
+                }
+            },
+            {
+                name: "TIPPED_ARROW",
+                skipSimple: true,
+                meta: {
+                    potion
+                }
+            },
+        ])
+    );
 }
 
 module.exports = { retrieve }
