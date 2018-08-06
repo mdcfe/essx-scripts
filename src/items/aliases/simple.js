c3dcac1641d7716453d2354560f221da63305095
md678685 <mathew678@hotmail.co.uk>
2018-08-06 08:41:46 +0000

    Include lowercased original material name as alias

--- src/items/aliases/simple.js
+++ src/items/aliases/simple.js
@@ -1,5 +1,8 @@
 module.exports = {
     prep: null,
     test: ({ skipSimple }) => !skipSimple,
-    get: ({ name }) => name.toLowerCase().replace(/_/g, "")
+    get: ({ name }) => [
+        name.toLowerCase(), // PINEAPPLE_PLANT -> pineapple_plant
+        name.toLowerCase().replace(/_/g, "") // PINEAPPLE_PLANT -> pineappleplant
+    ]
 }
