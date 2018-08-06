f1813b6846b9bb41ac5f7fa7683dd548c380b00f
md678685 <mathew678@hotmail.co.uk>
2018-08-06 14:57:03 +0100

    Add additional potion exclusions

--- test/data/itemExclusions.js
+++ test/data/itemExclusions.js
@@ -1,22 +1,25 @@
 const exclusions = [
-    "clear+pot",
-    "diffuse",
-    "artless",
-    "thin+pot",
-    "bungling",
-    "smooth+pot",
-    "suave",
-    "debonair",
-    "charming",
-    "refined",
-    "cordial",
-    "sparkling",
-    "potent",
-    "rank",
-    "acrid",
-    "gross",
-    "stinky",
-    "mundaneex",
+    "clear+pot", // Removed from game
+    "diffuse", // Removed from game
+    "artless", // Removed from game
+    "thin+pot", // Removed from game
+    "bungling", // Removed from game
+    "smooth+pot", // Removed from game
+    "suave", // Removed from game
+    "debonair", // Removed from game
+    "charming", // Removed from game
+    "refined", // Removed from game
+    "cordial", // Removed from game
+    "sparkling", // Removed from game
+    "potent", // Removed from game
+    "rank", // Removed from game
+    "acrid", // Removed from game
+    "gross", // Removed from game
+    "stinky", // Removed from game
+    "mundaneex", // Removed from game
+    "dualbit", // Removed from game
+    "regenextendepotion", // Typo
+    "slowextenedpotion", // Typo
 ]
 
 function matches(input, pattern) {
