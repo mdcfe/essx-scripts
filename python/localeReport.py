412cd677492201cdbc882dde5ef9d4cf601c8608
md678685 <mathew678@hotmail.co.uk>
2018-08-24 16:09:47 +0100

    Fix indentation in localeReport

--- python/localeReport.py
+++ python/localeReport.py
@@ -1,34 +1,34 @@
-	import os, re
+import os, re
 
 
-	base = "messages.properties"
+base = "messages.properties"
 
 
-	with open(base) as baseFile:
-	    baseKeys = re.findall(r"^([a-zA-z]+)=", baseFile.read(), re.MULTILINE)
+with open(base) as baseFile:
+    baseKeys = re.findall(r"^([a-zA-z]+)=", baseFile.read(), re.MULTILINE)
 
 
-	for filename in os.listdir("."):
-	    if filename.startswith("messages") and filename.endswith(".properties") and filename != base:
-	        with open(filename) as compareFile:
-	            compareKeys = re.findall(r"^([a-zA-z]+)=", compareFile.read(), re.MULTILINE)
+for filename in os.listdir("."):
+    if filename.startswith("messages") and filename.endswith(".properties") and filename != base:
+        with open(filename) as compareFile:
+            compareKeys = re.findall(r"^([a-zA-z]+)=", compareFile.read(), re.MULTILINE)
 
-	        missingKeys = [key for key in baseKeys if key not in compareKeys]
-	        extraKeys = [key for key in compareKeys if key not in baseKeys]
+        missingKeys = [key for key in baseKeys if key not in compareKeys]
+        extraKeys = [key for key in compareKeys if key not in baseKeys]
 
-	        print("## {}".format(filename))
-
-
-	        if len(missingKeys) > 0:
-	            print("### Missing keys ({})".format(len(missingKeys)))
-	            for k in missingKeys:
-	                print("* {}".format(k))
+        print("## {}".format(filename))
 
 
-	        if len(extraKeys) > 0:
-	            print("### Extra keys ({})".format(len(extraKeys)))
-	            for k in extraKeys:
-	                print("* {}".format(k))
+        if len(missingKeys) > 0:
+            print("### Missing keys ({})".format(len(missingKeys)))
+            for k in missingKeys:
+                print("* {}".format(k))
 
 
-	        print()
+        if len(extraKeys) > 0:
+            print("### Extra keys ({})".format(len(extraKeys)))
+            for k in extraKeys:
+                print("* {}".format(k))
+
+
+        print()
