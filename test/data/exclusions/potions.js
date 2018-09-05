56dd2229563322abcf83762ff9dfcea8ccb0ddf2
md678685 <mathew678@hotmail.co.uk>
2018-09-05 14:12:11 +0100

    Exclude removed double slabs

---
+++ test/data/exclusions/potions.js
@@ -0,0 +1,24 @@
+module.exports = [
+    "clear+pot", // Removed from game
+    "diffuse", // Removed from game
+    "artless", // Removed from game
+    "thin+pot", // Removed from game
+    "bungling", // Removed from game
+    "smooth+pot", // Removed from game
+    "suave", // Removed from game
+    "debonair", // Removed from game
+    "charming", // Removed from game
+    "refined", // Removed from game
+    "cordial", // Removed from game
+    "sparkling", // Removed from game
+    "potent", // Removed from game
+    "rank", // Removed from game
+    "acrid", // Removed from game
+    "gross", // Removed from game
+    "stinky", // Removed from game
+    "mundaneex", // Removed from game
+    "dualbit", // Removed from game
+    "db+pot", // Removed from game
+    "regenextendepotion", // Typo
+    "slowextenedpotion", // Typo
+]
--- test/data/itemExclusions.js
+++ test/data/itemExclusions.js
@@ -1,26 +1,6 @@
 const exclusions = [
-    "clear+pot", // Removed from game
-    "diffuse", // Removed from game
-    "artless", // Removed from game
-    "thin+pot", // Removed from game
-    "bungling", // Removed from game
-    "smooth+pot", // Removed from game
-    "suave", // Removed from game
-    "debonair", // Removed from game
-    "charming", // Removed from game
-    "refined", // Removed from game
-    "cordial", // Removed from game
-    "sparkling", // Removed from game
-    "potent", // Removed from game
-    "rank", // Removed from game
-    "acrid", // Removed from game
-    "gross", // Removed from game
-    "stinky", // Removed from game
-    "mundaneex", // Removed from game
-    "dualbit", // Removed from game
-    "db+pot",
-    "regenextendepotion", // Typo
-    "slowextenedpotion", // Typo
+    ...require("./exclusions/potions"),
+    ...require("./exclusions/dslabs"),
 ]
 
 function matches(input, pattern) {
