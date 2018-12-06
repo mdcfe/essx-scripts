c1c62e2ae853b094195954621157dc208ad33cd5
md678685 <mathew678@hotmail.co.uk>
2018-12-06 19:02:55 +0000

    Add some fixed aliases

--- src/items/aliases/fixed.js
+++ src/items/aliases/fixed.js
@@ -36,6 +36,7 @@ const aliases = {
     CHISELED_RED_SANDSTONE: ["crstone", "redsandstonechiseled", "chiseledredsandstone"],
     CUT_RED_SANDSTONE: ["srstone", "redsandstonesmooth", "smoothredsandstone"],
     COBWEB: ["spiderweb", "sweb", "cweb", "web"],
+    IRON_BARS: ["ironbarsb", "ironbarsblock", "ironfence", "metalbars", "metalbarsb", "metalbarsblock", "metalfence", "jailbars", "jailbarsb", "jailbarsblock", "jailfence", "mbars", "mbarsb", "mbarsblock", "mfence", "jbars", "jbarsb", "jbarsblock", "ibars", "ibarsb", "ibarsblock", "ifence"],
     // == Plants (not crops) ==
     TALL_GRASS: ["longgrass", "wildgrass", "grasslong", "grasstall", "grasswild", "lgrass", "tgrass", "wgrass"],
     DEAD_BUSH: ["bush", "deadshrub", "dshrub", "dbush", "deadsapling"],
@@ -49,6 +50,9 @@ const aliases = {
     PINK_TULIP: ["tulippink", "ptulip", "tulipp"],
     ORANGE_TULIP: ["tuliporange", "otulip", "tulipo"],
     OXEYE_DAISY: ["oxeye", "daisy", "daisyoxeye", "moondaisy", "daisymoon", "lightgrayoxeye", "lgrayoxeye", "lightgreyoxeye", "lgreyoxeye"],
+    // == Crops ==
+    CARVED_PUMPKIN: ["hollowpumpkin", "cutpumpkin", "oldpumpkin", "legacypumpkin"],
+    JACK_O_LANTERN: ["pumpkinlantern", "glowingpumpkin", "lightpumpkin", "jpumpkin", "plantren", "glowpumpkin", "gpumpkin", "lpumpkin"],
     // == End Materials ==
     END_PORTAL: ["endergoo", "enderportal", "endgoo", "eportal", "egoo"],
     END_PORTAL_FRAME: ["endergooframe", "enderportalframe", "endgooframe", "eportalframe", "egooframe", "enderframe", "endframe"],
