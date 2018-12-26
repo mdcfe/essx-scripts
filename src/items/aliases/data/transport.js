df618205bda79c2c2f2526d81588bb6b7bf8e7b7
md678685 <mathew678@hotmail.co.uk>
2018-12-26 20:13:20 +0000

    Add new minecart aliases
    73% woo

--- src/items/aliases/data/transport.js
+++ src/items/aliases/data/transport.js
@@ -4,13 +4,21 @@ const prefixes = {
     ACTIVATOR: ["activator", "activate", "trigger", "a", "t"],
     DETECTOR: ["detector", "detecting", "detect", "d"],
     POWERED: ["powered", "booster", "power", "boost", "p", "b"],
-    // Basic rails don't have a prefix
-    RAIL: ["", "minecart", "mcart", "mc"]
+    // These should all be preferred over basic minecarts
+    TNT: ["tnt", "dynamite", "bomb", "t", "d", "b"],
+    HOPPER: ["hopper", "hop", "h"],
+    CHEST: ["storage", "chest", "s", "c"],
+    COMMAND_BLOCK: ["cmdblock", "cblock", "command", "cmd", "cb", "commandblock"],
+    FURNACE: ["engine", "powered", "power", "furnace", "e", "p", "f"],
+    // Basic rails/minecarts don't have a prefix
+    RAIL: ["", "minecart", "mcart", "mc"],
+    MINECART: [""],
 };
 
 // There's only one rail type here
 const suffixes = {
-    RAIL: ["rails", "rail", "track"]
+    RAIL: ["rails", "rail", "track"],
+    MINECART: ["minecart", "mcart", "mc", "cart"],
 }
 
 // Certain full names that shouldn't be outputted as aliases here, as they refer to another material
