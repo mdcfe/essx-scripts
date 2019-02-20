370b2e08cb164b438da93a696f75994b2eb507ae
md678685 <mathew678@hotmail.co.uk>
2019-02-20 14:43:45 +0000

    Update stone aliases
    Add red sandstone, as well as stairs, slabs and smooth/cut variants

--- src/items/aliases/data/stone.js
+++ src/items/aliases/data/stone.js
@@ -1,40 +1,49 @@
 // Stone brick variations
-const prefixes = {
+const suffixes = {
     // These should all be preferred over stone brick variations
     // These will be populated later
     INFESTED_CHISELED: [],
     INFESTED_CRACKED: [],
     INFESTED_MOSSY: [],
-    INFESTED: ["silverfish", "sfish", "fish", "infested", "monsteregg", "megg", "trap", "sf", "me"],
+    // This is used by the code below but not directly used for generating aliases
+    INFESTED: ["!silverfish", "!sfish", "!fish", "!infested", "!monsteregg", "!megg", "!trap", "!sf", "!me"],
     // These should all be preferred over normal stone bricks
-    CHISELED: ["chiseled", "circle", "ci"],
-    CRACKED: ["cracked", "crack", "cr", "c"],
-    MOSSY: ["mossy", "moss", "m"],
+    CHISELED: ["!chiseled", "!circle", "!ci"],
+    CRACKED: ["!cracked", "!crack", "!cr", "!c"],
+    MOSSY: ["!mossy", "!moss", "!m"],
+    // These are sandstone variations
+    SMOOTH: ["!smooth", "!sm"],
+    CUT: ["!cut"],
+    // These are actually suffixes here
+    SLAB: ["slab", "sl"],
+    STAIR: ["stair", "stairs", "st"],
     // Normal stone blocks have no prefix
-    STONE: [""]
+    $: [""]
 };
 
-Object.keys(prefixes)
-    .filter(prefix => prefix.includes("INFESTED_"))
-    .forEach(prefix => {
-        basePrefix = prefix.substr(9);
-        base = prefixes[basePrefix];
+Object.keys(suffixes)
+    .filter(suffix => suffix.includes("INFESTED_"))
+    .forEach(suffix => {
+        baseSuffix = suffix.substr(9);
+        base = suffixes[baseSuffix];
 
-        prefixes.INFESTED.forEach(x => {
+        suffixes.INFESTED.forEach(x => {
             base.forEach(y => {
-                prefixes[prefix].push(`${x}${y}`);
+                y = y.replace("!", "");
+                suffixes[suffix].push(`${x}${y}`);
             });
         });
     });
 
-const suffixes = {
+const prefixes = {
     // Match first
     COBBLESTONE_WALL: ["cobblestonewall", "cobblewall", "cstonewall", "cswall", "cwall", "cobblestonefence", "cobblefence", "cstonefence", "csfence", "cfence", "stonewall", "swall"],
     STONE_BRICKS: ["stonebrick", "stonebrickblock", "stonebb", "sbrick", "sbricks", "sbrickblock", "stonebricks", "sbb"],
     // Rest of the aliases
-    _COBBLESTONE: ["cobblestone", "cstone", "cobble"],
-    _SANDSTONE: ["sandstone"],
-    _STONE: ["stone", "smoothstone", "sstone"],
+    COBBLESTONE: ["cobblestone", "cstone", "cobble"],
+    RED_SANDSTONE: ["redsandstone", "rsandstone", "rsandst", "rsastone"],
+    SANDSTONE: ["sandstone", "sandst", "sastone"],
+    STONE: ["stone", "smoothstone", "sstone"],
 }
 
 // Certain full names that shouldn't be outputted as aliases here, as they refer to another material
